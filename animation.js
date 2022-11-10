90c810767f8bf7129828d9535a5bfbe9d3a9f9af
Cegeeenre <73913043+Cegeeenre@users.noreply.github.com>
2022-11-10 18:05:31 +0100

    finish

--- animation.js
+++ animation.js
@@ -6,14 +6,13 @@ let b = el.getBoundingClientRect();
 el.addEventListener("mousemove", (e) => {
     // get mouse position relative to the element
     let X = (e.clientX - b.left) / w ;
-    let Y = (e.clientY - b.top) / h ;
+    let Y = e.clientY / h ;
 
     // get the mouse for the blare effect
     let rX = -(X - 0.5) * 26;
     let rY = (Y - 0.5) * 26;
 
-
-    console.log(X, Y);
+    // console.log(X, Y);
     // Movement of the card
     document.documentElement.style.setProperty("--x", 100 * X + "%");
     document.documentElement.style.setProperty("--y", 100 * Y + "%");
@@ -21,4 +20,5 @@ el.addEventListener("mousemove", (e) => {
     // Blare effect
     document.documentElement.style.setProperty("--r-x", rX + "deg");
     document.documentElement.style.setProperty("--r-y", rY + "deg");
+
 });
